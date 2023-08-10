ca1f9db1fbfbbd687684656fbfaf4c1264639df2
Edward Tanguay (windows) <edward@tanguay.info>
2023-08-10 11:55:08 +0200

    with one parameter

--- cli/cp.js
+++ cli/cp.js
@@ -1,1 +1,18 @@
-console.log('works')
+import fs from 'fs';
+
+const title = process.argv[2];
+
+const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>${title}</title>
+</head>
+<body>
+	<h1>${title}</h1>
+	<p>Welcome to this page.</p>
+</body>
+</html>`;
+
+fs.writeFileSync('index.html', html, { encoding: 'utf8' });
